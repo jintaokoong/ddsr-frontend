f16e96935605aebbd18f0df3cc0f9b10493ce96f
Jintao Koong <jintaokoong99@gmail.com>
2021-12-14 17:25:55 +0800

    🚧  add start stop button

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from "preact/hooks";
 import { css } from "@emotion/css";
 import apiService from "./services/api-service";
+import { FaPlay, FaStop } from "react-icons/fa";
 
 const mainStyle = css`
   max-width: 420px;
@@ -33,18 +34,39 @@ const slicedLabelStyle = css`
 `;
 
 const headerInputStyle = css`
-  box-sizing: border-box;
-  width: 100%;
-  height: 30px;
+  flex: 1;
+  height: 35px;
+  margin-right: 5px;
   border-width: 2px;
   border-color: #004f8f;
   padding: 0 10px;
   border-radius: 3px;
-  max-width: calc(100% - 20px);
-  margin: 10px;
+`;
+
+const buttonStyle = css`
+  border: 0;
+  border-radius: 3px;
+  color: white;
+  min-width: 40px;
+`;
+
+const startButtonStyle = css`
+  ${buttonStyle}
+  background-color: green;
+  :active {
+    background-color: darkgreen;
+  }
+`;
+const stopButtonStyle = css`
+  ${buttonStyle}
+  background-color: red;
+  :active {
+    background-color: darkred;
+  }
 `;
 
 export function App() {
+  const [start, setStart] = useState(false);
   const [grouping, setGrouping] = useState<Record<string, any[]>>({});
   const [name, setName] = useState("");
   useEffect(() => {
@@ -68,26 +90,33 @@ export function App() {
 
   return (
     <main className={mainStyle}>
-      <input
-        placeholder={"手動加歌"}
-        className={headerInputStyle}
-        value={name}
-        onInput={(e) => setName(e.currentTarget.value)}
-        onKeyPress={(e) => {
-          if (e.key !== "Enter") {
-            return;
-          }
-          apiService
-            .createRequest(name)
-            .then(() =>
-              apiService.getRequests().then((gps) => setGrouping(gps))
-            )
-            .catch((err) => console.error("post-create", err));
-          setName("");
-
-          // do api call
-        }}
-      />
+      <div style={{ display: "flex", padding: 10 }}>
+        <input
+          className={headerInputStyle}
+          placeholder={"手動加歌"}
+          value={name}
+          onInput={(e) => setName(e.currentTarget.value)}
+          onKeyPress={(e) => {
+            if (e.key !== "Enter") {
+              return;
+            }
+            apiService
+              .createRequest(name)
+              .then(() =>
+                apiService.getRequests().then((gps) => setGrouping(gps))
+              )
+              .catch((err) => console.error("post-create", err));
+            setName("");
+          }}
+        />
+        <button
+          className={!start ? startButtonStyle : stopButtonStyle}
+          type={"button"}
+          onClick={() => setStart((s) => !s)}
+        >
+          {!start ? <FaPlay /> : <FaStop />}
+        </button>
+      </div>
       {Object.keys(grouping).map((k) => (
         <div key={k}>
           <h4 className={headerStyle}>{k}</h4>
